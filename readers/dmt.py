9dd2c66c233a841174b5185ea3914369ff12bd36
Graeme Nott <graeme.nott@faam.ac.uk>
2017-03-27 18:48:16 +0100

    Seperated functions for PADS2.x and PADS3.s format files

--- readers/dmt.py
+++ readers/dmt.py
@@ -4,7 +4,7 @@ python:   3.4
 author:   Graeme Nott
 email:    graeme.nott@faam.ac.uk
 created:  Mar 2017
-modified: 
+modified:
 
 File with Droplet Measurement Technologies instrument data readers
 """
@@ -13,27 +13,69 @@ import numpy as np
 
 import pdb
 
-def CIPgs():
+
+def CIPgs_PADS2():
     """
     Reader function for a DMT Cloud Imaging Probe - Grayscale
+    PADS version 2.x
+
+    Input args:
+
     """
 
-    print('CIPgs')
+    print('CIPgs_PADS2')
 
     return
 
-def CIP():
+
+def CIPgs_PADS3():
+    """
+    Reader function for a DMT Cloud Imaging Probe - Grayscale
+    PADS version 3.x
+
+    Input args:
+
+    """
+
+    print('CIPgs_PADS3')
+
+    return
+
+
+def CIP_PADS2():
     """
     Reader function for a DMT Cloud Imaging Probe - Monoscape
+    PADS version 2.x
+
+    Input args:
+
     """
 
-    print('CIP')
+    print('CIP_PADS2')
 
     return
 
+
+def CIP_PADS3():
+    """
+    Reader function for a DMT Cloud Imaging Probe - Monoscape
+    PADS version 3.x
+
+    Input args:
+
+    """
+
+    print('CIP_PADS3')
+
+    return
+
+# ----------------------------------------------------------------------
 def DMT_lib():
     """
     Reader library of universal functions for DMT probes
+
+    Input args:
+        ver:       version of PADS files
     """
 
     print('DMT_lib')
