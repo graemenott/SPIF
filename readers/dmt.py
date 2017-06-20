e3b51540e8fb12067d10757169402d4f901fc72b
Graeme Nott <graeme.nott@faam.ac.uk>
2017-06-20 22:30:36 +0100

    Reordered lib func. Added args and basic return functionality to reader functions

--- readers/dmt.py
+++ readers/dmt.py
@@ -4,7 +4,6 @@ python:   3.4
 author:   Graeme Nott
 email:    graeme.nott@faam.ac.uk
 created:  Mar 2017
-modified:
 
 File with Droplet Measurement Technologies instrument data readers
 """
@@ -14,62 +13,6 @@ import numpy as np
 import pdb
 
 
-def CIPgs_PADS2():
-    """
-    Reader function for a DMT Cloud Imaging Probe - Grayscale
-    PADS version 2.x
-
-    Input args:
-
-    """
-
-    print('CIPgs_PADS2')
-
-    return
-
-
-def CIPgs_PADS3():
-    """
-    Reader function for a DMT Cloud Imaging Probe - Grayscale
-    PADS version 3.x
-
-    Input args:
-
-    """
-
-    print('CIPgs_PADS3')
-
-    return
-
-
-def CIP_PADS2():
-    """
-    Reader function for a DMT Cloud Imaging Probe - Monoscape
-    PADS version 2.x
-
-    Input args:
-
-    """
-
-    print('CIP_PADS2')
-
-    return
-
-
-def CIP_PADS3():
-    """
-    Reader function for a DMT Cloud Imaging Probe - Monoscape
-    PADS version 3.x
-
-    Input args:
-
-    """
-
-    print('CIP_PADS3')
-
-    return
-
-# ----------------------------------------------------------------------
 def DMT_lib():
     """
     Reader library of universal functions for DMT probes
@@ -81,3 +24,78 @@ def DMT_lib():
     print('DMT_lib')
 
     return
+
+
+# ----------------------------------------------------------------------
+def CIPgs_PADS2(fin,d=None):
+    """
+    Reader function for a DMT Cloud Imaging Probe - Grayscale
+    PADS version 2.x
+
+    Input args:
+
+    """
+
+    print('reader: CIPgs_PADS2')
+
+    if d is None:
+        # Create a bare-bones object if one has not been passed
+        d = spif.Spif()
+
+    return d
+
+
+def CIPgs_PADS3(fin,d=None):
+    """
+    Reader function for a DMT Cloud Imaging Probe - Grayscale
+    PADS version 3.x
+
+    Input args:
+
+    """
+
+    print('reader: CIPgs_PADS3')
+
+    if d is None:
+        # Create a bare-bones object if one has not been passed
+        d = spif.Spif()
+
+    return d
+
+
+
+def CIP_PADS2(fin,d=None):
+    """
+    Reader function for a DMT Cloud Imaging Probe - Monoscape
+    PADS version 2.x
+
+    Input args:
+
+    """
+
+    print('reader: CIP_PADS2')
+
+    if d is None:
+        # Create a bare-bones object if one has not been passed
+        d = spif.Spif()
+
+    return d
+
+
+def CIP_PADS3(fin,d=None):
+    """
+    Reader function for a DMT Cloud Imaging Probe - Monoscape
+    PADS version 3.x
+
+    Input args:
+
+    """
+
+    print('reader: CIP_PADS3')
+
+    if d is None:
+        # Create a bare-bones object if one has not been passed
+        d = spif.Spif()
+
+    return d
+
