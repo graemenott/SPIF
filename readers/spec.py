911205af8e1924e8097483da057989b6261801d0
Graeme Nott <graeme.nott@faam.ac.uk>
2017-06-26 13:06:41 +0100

    Updated docstrings to Google format

--- readers/spec.py
+++ readers/spec.py
@@ -4,9 +4,26 @@ python:   3.4
 author:   Graeme Nott
 email:    graeme.nott@faam.ac.uk
 created:  Mar 2017
-modified: 
 
-File with SPEC Inc. instrument data readers
+File with SPEC Inc. instrument data readers.
+
+Reader functions for SPEC Inc instruments that read raw particle
+image files and produce a spif.Spif() instance.
+
+Requirements, reserved keys, and writer behaviours:
+
+- The 'ancillary_variables' key is used to link one dataset to another.
+This is usually a dataset of errors/uncertainties of a dataset. The
+value of this item should be the key of the linked dataset.
+- None values will be converted to the _FillValue if this attribute has
+been set for the dataset, numpy.nan for a dataset which has units (and
+thus is assumed to be a number), or an empty string otherwise.
+
+Notes:
+Currently all h5 dtypes are handled automatically. Thus the dtype of a
+dataset should be given explicitly in the numpy array.
+
+
 """
 
 import numpy as np
@@ -28,6 +45,12 @@ def SPEC_lib():
 def twoDS(fin,d=None):
     """
     Reader function for a SPEC 2D Stereo probe
+
+    Args:
+        fin (str or pathlib.path object): Path/file of a raw data file.
+        d (spif.Spif): If None [default] then create spif.Spif instance
+            with data. If given then append/insert data into d
+
     """
 
     print('reader: twoDS')
@@ -42,6 +65,12 @@ def twoDS(fin,d=None):
 def CPI(fin,d=None):
     """
     Reader function for a SPEC Cloud Particle Imager
+
+    Args:
+        fin (str or pathlib.path object): Path/file of a raw data file.
+        d (spif.Spif): If None [default] then create spif.Spif instance
+            with data. If given then append/insert data into d
+
     """
 
     print('reader: CPI')
@@ -56,6 +85,12 @@ def CPI(fin,d=None):
 def HVPS(fin,d=None):
     """
     Reader function for a SPEC High Volume Precipitation Spectrometer
+
+    Args:
+        fin (str or pathlib.path object): Path/file of a raw data file.
+        d (spif.Spif): If None [default] then create spif.Spif instance
+            with data. If given then append/insert data into d
+
     """
 
     print('reader: HVPS')
