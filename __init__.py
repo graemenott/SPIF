3faf30adec949a61d71ffcf65d0fffcb0c6a290f
Graeme Nott <graeme.nott@faam.ac.uk>
2021-03-03 11:17:33 +0000

    Added project parameter variables

--- __init__.py
+++ __init__.py
@@ -1,2 +1,11 @@
 
-# __init__.py for spif package root
+# spif package root
+
+__title__ = 'SPIF - Single Particle Image Format'
+__description__ = 'SPIF convention definitions documentation'
+__author__ = 'the SPIF inititive' 
+__copyright__ = ''
+__date__ = '2021 03 03'
+__version_major__ = 0
+__version_minor__ = 86
+
