e300648353977f31ef7860fd3e17818723810ea6
Graeme Nott <graeme.nott@faam.ac.uk>
2021-03-03 15:02:30 +0000

    Added some imports

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@
 
 __title__ = 'SPIF - Single Particle Image Format'
 __description__ = 'SPIF convention definitions documentation'
-__author__ = 'the SPIF inititive' 
-__copyright__ = ''
+__author__ = 'the SPIF inititive'
+__copyright__ = 'undecided'
 __date__ = '2021 03 03'
 __version_major__ = 0
 __version_minor__ = 86
 
+#from . import spif
+#from . import readers
