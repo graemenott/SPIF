78d33e62ce4b31d9ce854dda561f1a4c4307ea35
Graeme Nott <graeme.nott@faam.ac.uk>
2021-03-14 16:19:36 +0000

    Initial commit (from other branch)

---
+++ spif/__init__.py
@@ -0,0 +1,18 @@
+
+# spif package root
+
+__title__ = 'SPIF - Single Particle Image Format'
+__description__ = 'SPIF convention definitions documentation'
+__author__ = 'the SPIF inititive'
+__copyright__ = 'undecided'
+__date__ = '2021 03 03'
+__version_major__ = 0
+__version_minor__ = 86
+
+#import pdb
+#pdb.set_trace()
+
+#from . import csv2rst
+#from . import spif
+#from spif import csv2rst
+#from . import readers
