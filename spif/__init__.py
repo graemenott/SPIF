1b23033cdc7a9a34298a1a858a1d5e5fb6335f70
Graeme Nott <graeme.nott@faam.ac.uk>
2021-11-22 08:53:00 +0000

    updated version

--- spif/__init__.py
+++ spif/__init__.py
@@ -5,9 +5,9 @@ __title__ = 'SPIF - Single Particle Image Format'
 __description__ = 'SPIF convention definitions documentation'
 __author__ = 'the SPIF inititive'
 __copyright__ = 'undecided'
-__date__ = '2021 03 03'
+__date__ = '2021 11 18'
 __version_major__ = 0
-__version_minor__ = 8.6
+__version_minor__ = 9
 
 
 from .compliance_checker import *
