0d7aca7db729cf1addc409c9dbcdee6c80fbec7b
Graeme Nott <graeme.nott@faam.ac.uk>
2021-11-23 19:06:14 +0000

    Added multiple file imports and added general compliance_checker attributes

--- spif/compliance_checker/__init__.py
+++ spif/compliance_checker/__init__.py
@@ -1,4 +1,12 @@
 
 # spif.compliance_checker package
 
+
+__author__ = 'graeme.nott@faam.ac.uk'
+__date__ = '2021 11 22'
+__version__ = '0.2'
+
+
 from .param_parser import *
+from .param_docsrc import *
+from .param_check import *
